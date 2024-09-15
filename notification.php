cd4b40787f44feec762cfc688d20e2f84270ac86
Natalie-codeNovice <anathaliemukash@gmail.com>
2024-09-15 09:49:56 +0200

    fdf

--- notification.php
+++ notification.php
@@ -62,12 +62,12 @@ include "./includes/header.php";
                 
                 <div class="form-group">
                   <label for="subject">Subject:</label>
-                  <input type="text" class="form-control" id="subject" name="subject" placeholder="Enter subject">
+                  <input type="text" required class="form-control" id="subject" name="subject" placeholder="Enter subject">
                 </div>
 
                 <div class="form-group">
                   <label for="message">Message:</label>
-                  <textarea class="form-control" id="message" name="message" rows="4" placeholder="Enter your message"></textarea>
+                  <textarea class="form-control" required id="message" name="message" rows="4" placeholder="Enter your message"></textarea>
                 </div>
 
                 <button type="submit" class="btn btn-primary">Send Email</button>
